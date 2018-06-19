1ca7dd4a57cf924832bfe93d50a5fb26da8d7449
lkaba2003 <lkaba2003@yahoo.com>
2018-06-19 10:24:18 -0400

    Tranfered day1Demo in new folder(lamine_kaba)

---
+++ Lamine_Kaba/test/src/test/Test.java
@@ -0,0 +1,74 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		
+		System.out.println(x++ * ++x * x++);
+	}
+
+}
+
+/*
+ * JDV -> JRE -> JVM
+ * JVM = Java Virtual Machine
+ * Change ButeCode to Machine Lang
+ * Lives in the JRE
+ * 
+ * JRE = Java Runtime Env
+ * A Collection of jar file/ libraries
+ * needed to run all java apps
+ * Contains the JVM
+ * 
+ * JDK = Java Development Kit
+ * Javac (Compliler)
+ * Contains the JRE
+ */
+
+/*
+ * AutoBoxing is using primitive class to declare a variable
+ * Integer X = 5;
+ * Unboxing is using the short name as type of a variable
+ * Int x = 5;
+ */
+
+/*
+ * Clases vs Object?
+ * 
+ * Class is blue print or template that define the sates(fields) and behaviors(
+ * methods) of an object.
+ * Object is an instance of the class
+ */
+
+/*
+ * Methods vs Function
+ * Methods live in classes and Function do not
+ */
+/* Packages vs Imports
+ * package are groupings of code file related functionality
+ * imports are used to make other classes usable with in a package
+ */
+/*
+ * Variable declarations vs initializations
+ * int x; (declaration)
+ * x=0; (initialization)
+ * x=1; (re-assignment)
+ */
+
+/*Instance vs Class members
+ * Class members are static
+ * Instance members are non-static
+ */
+
+
+
+
+
+
+
+
+
+
