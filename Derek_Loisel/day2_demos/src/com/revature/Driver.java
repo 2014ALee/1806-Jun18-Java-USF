258d5ac7975bde990c46beaca9c899ff2008cf9a
Derek Loisel <djloisel19@gmail.com>
2018-06-19 12:01:11 -0400

    commiting day 2 demos before lunch

---
+++ Derek_Loisel/day2_demos/src/com/revature/Driver.java
@@ -0,0 +1,30 @@
+package com.revature;
+
+import com.revature.models.Person;
+import com.revature.models.Skill;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		
+		System.out.println(person1.getFirstName()); //gives us null because value hasnt been set
+		//System.out.println(person1.getFirstName().toUpperCase()); //gives a null point exception because its invoking a method on a null object
+	
+		person1.setAge(29);
+		person1.setFirstName("Derek");
+		person1.setLastName("Loisel");
+		person1.setPrimarySkill(Skill.JAVA);
+		
+		System.out.println(person1); //doesnt need to call overridded toString because its implicit in println
+	
+	Person person2 = new Person("Wezley", "Singleton", 27, Skill.JAVA);
+	
+	Person person3 = new Person("Derek", "Loisel", 29, Skill.JAVA);
+	
+	System.out.println(person1 == person3); // false because just references memory location
+	System.out.println(person1.equals(person3)); //true because its checking the actual values with the overridden hash and equals, if not overriden it behaves same as ==
+	}
+	
+}
---
+++ Derek_Loisel/day2_demos/src/com/revature/ScopeDriver.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+public class ScopeDriver {
+
+	public static int x = 0;
+	
+	public static void Main(String[] args) {
+		
+		System.out.println(x); //prints 0
+		System.out.println(changeX(x)); //prints 1
+		System.out.println(x); //prints 0 because the value is passed, but x itself isnt changed unless you set x equal to value in the changeX method
+	}
+	
+	public static int changeX(int value) {
+		return ++value;
+	}
+}
