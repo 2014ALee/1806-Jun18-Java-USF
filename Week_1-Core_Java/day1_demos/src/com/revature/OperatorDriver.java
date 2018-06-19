8effeda29ed8561b1be3ef7a55cf5fbada66f254
wsingleton <wezley.singleton@gmail.com>
2018-06-18 20:18:14 -0400

    Adding comments and additional notes to demos

--- Week_1-Core_Java/day1_demos/src/com/revature/OperatorDriver.java
+++ Week_1-Core_Java/day1_demos/src/com/revature/OperatorDriver.java
@@ -3,6 +3,8 @@ package com.revature;
 import java.util.ArrayList;
 import java.util.List;
 
+// The import below does not work because of a naming collision (which List would the compiler use?)
+//import java.awt.List;
 
 public class OperatorDriver {
 	
@@ -75,28 +77,30 @@ public class OperatorDriver {
 		
 		// division
 		double myQuotient = myDifference / mySum;
-		System.out.println(myQuotient); // this doesn't work properly...
+		System.out.println(myQuotient); // this doesn't work properly, because our operands are ints! (The compiler is defaulting to integer division)
 		
 		myQuotient = 2 / 9;
 		System.out.println(myQuotient); // this doesn't work
 		
 		myQuotient = 2d / 9d;
-		System.out.println(myQuotient);
+		System.out.println(myQuotient); // explicitly declaring the digits as double literals works!
 		
-		myQuotient = 2. / 9.;
-		System.out.println(myQuotient);
+		myQuotient = 2. / 9.; // digits are apparently optional after the decimal point
+		System.out.println(myQuotient); // including the decimal places tells the compiler to treat these as floating point numbers
 		
 		myQuotient = 2. / 9;
-		System.out.println(myQuotient);
+		System.out.println(myQuotient); // we really only need to declare one of the operands as a double to get the compiler to do floating point division
 		
 		myQuotient = (double) myDifference / mySum;
-		System.out.println(myQuotient);
-		
+		System.out.println(myQuotient); // to have compiler treat already declared variables differently, cast it as the type you need
+
+		// REMEMBER TO BEWARE OF TRYING TO CONVERT LARGER DATA TYPES INTO SMALLER ONES!!!
 		int myValue = 0;
 		long myLongValue = 345345348768734345L;
 		myValue = (int) myLongValue;
 		System.out.println(myValue);
 		
+		// modulus operator (remainder)
 		double myRemainder = 12 % 5;
 		System.out.println(myRemainder);
 		
@@ -111,17 +115,22 @@ public class OperatorDriver {
 		
 		ArrayList<String> myArrayList = new ArrayList<>(); 
 		
+		// Greater-than, Less-than, Greater-than-or-equal, Less-than-or-equal 
 		System.out.println(a > b);
 		System.out.println(a < b);
 		System.out.println(c < d);
 		System.out.println(c <= d);
 		System.out.println(d >= a);
-		System.out.println("------------");
+		System.out.println("------------"); // separator
+		
+		// instanceof returns true if the first operand is a type of the second operand
 		System.out.println(myStr instanceof String);
-		System.out.println(myStr instanceof Object);
+		System.out.println(myStr instanceof Object); // since all Java objects directly/indirectly extend Object - this is true!
 		System.out.println(myArrayList instanceof ArrayList);
-		System.out.println(myArrayList instanceof List);
-		System.out.println("------------");
+		System.out.println(myArrayList instanceof List); // this returns true, even though List is an interface!
+		System.out.println("------------"); // separator
+		
+		// is-equal, not-equal
 		System.out.println(c == d);
 		System.out.println(a != b);
 		System.out.println("------------");
@@ -148,6 +157,18 @@ public class OperatorDriver {
 		
 		/*
 		 * Bitwise Operators (Short-Circuit Operators)
+		 * 
+		 * These are similar to the logical operators, and actually give the same results as their counterparts. However,
+		 * bitwise operators only check what is necessary to return a value. 
+		 * 
+		 * For instance, with AND - we know that both operands have to be true in order for the result to be true. So, if 
+		 * the first operand evaluated is false - then we already know that the result is going to be false.
+		 * 
+		 * Likewise, with OR - the operation will return true if at least one of the operands is true. So, if the value of 
+		 * the first operand is true, then there is no need to check the other.
+		 * 
+		 * XOR is a bit different, it is also known as the Exclusive-OR. This operator returns true if exactly one operand
+		 * has a true value. If both are true, then it will return false. Because of its nature, XOR must check both operands.
 		 */
 		// bitwise AND
 		result = value2 & value1;
@@ -161,9 +182,10 @@ public class OperatorDriver {
 		result = value1 ^ value3;
 		System.out.println(result);
 		
-		int myBinary = 0b0010; //base 2
-		int myOctal = 010; // base 8
-		int myHex = 0x3f; // base 16
+		// declaring values in different bases
+		int myBinary = 0b0010; // binary (base 2)
+		int myOctal = 010; // octal (base 8)
+		int myHex = 0x3f; // hexadecimal (base 16)
 		
 		System.out.println(myBinary);
 		System.out.println(myOctal);
@@ -171,6 +193,9 @@ public class OperatorDriver {
 		
 		/*
 		 * Assignment Operators
+		 * 
+		 * In addition to the simple assignment that we have been using during variable initialization, we also have the compound
+		 * assignment operators at our disposal. These include compound assignment operators for arithmetic and bitwise logical operators.
 		 */
 		// simple assignment
 		int z = 54;
@@ -194,7 +219,6 @@ public class OperatorDriver {
 		// mod-equals
 		z %= 6;
 		System.out.println(z);
-		
 		
 	}
 
