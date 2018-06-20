feeed7efe1a8061c4c86a9be2306529d03560157
lkaba2003 <lkaba2003@yahoo.com>
2018-06-19 23:13:22 -0400

    Checking if STS is Saving src files

---
+++ Lamine_Kaba/test2/src/test2/Myclass.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 0;
+		
+		for(i = 0; i <10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
