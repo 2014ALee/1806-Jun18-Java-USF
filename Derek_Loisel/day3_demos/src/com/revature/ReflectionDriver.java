4696e64e35a6b1ce9527518be42ef367e43c71a2
Derek Loisel <djloisel19@gmail.com>
2018-06-20 12:55:02 -0400

    commit after deleting johns folder from my branch

--- Derek_Loisel/day3_demos/src/com/revature/ReflectionDriver.java
+++ Derek_Loisel/day3_demos/src/com/revature/ReflectionDriver.java
@@ -5,7 +5,7 @@ public class ReflectionDriver {
 		/*
 		 * reflection provides ability to inspect and modify the runtime behavior of an application.  it can look at itself and inspect things like classes, interfaces, enums, fields , methods, constructors, etc. within itself
 		 */
-		
-		//wezleys example of changing the string class access modifiers and making strings changeable and changing the value of the string
-}
+
+		//Wezleys example of changing the string class access modifiers and making strings changeable and changing the value of the string
+	}
 }
