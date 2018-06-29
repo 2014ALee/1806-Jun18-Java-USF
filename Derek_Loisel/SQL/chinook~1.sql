0a07de9fd72be8e5cd8454c5b7427a1538026e0a
Derek Loisel <djloisel19@gmail.com>
2018-06-29 01:29:36 -0400

    commit after working on sql worksheet

--- Derek_Loisel/SQL/chinook~1.sql
+++ Derek_Loisel/SQL/chinook~1.sql
@@ -134,19 +134,126 @@ END;
 
 --SYSTEM DEFINED AGGREGATE FUNCTIONS
 --Task – Create a function that returns the average total of all invoices
+CREATE OR REPLACE FUNCTION get_avg_invoices
+RETURN NUMBER 
+AS 
+avg_invoices NUMBER; 
+BEGIN
+    SELECT AVG(total)
+    INTO avg_invoices
+    FROM invoice;
+    RETURN avg_invoices;
+END get_avg_invoices;
+/ 
+
+DECLARE 
+    avg_invoices NUMBER;
+BEGIN
+    avg_invoices := get_avg_invoices();
+    DBMS_OUTPUT.PUT_LINE('The average total of invoices: ' || avg_invoices);/*like a sysou, the || is to concatenate*/
+END;
+/
 
 --Task – Create a function that returns the most expensive track
+CREATE OR REPLACE FUNCTION get_most_expensive_track
+RETURN NUMBER 
+as
+most_expensive_track number;
+BEGIN
+    SELECT MAX (unitprice)
+    INTO most_expensive_track
+    FROM track;
+    
+    RETURN most_expensive_track;
+END get_most_expensive_track;
+/ 
 
+DECLARE 
+    most_expensive_track NUMBER;
+BEGIN
+    most_expensive_track := get_most_expensive_track();
+    DBMS_OUTPUT.PUT_LINE('The most expensive track: ' || most_expensive_track);
+END;
+/
 --USER DEFINED SCALAR FUNCTIONS
 --Task – Create a function that returns the average price of invoiceline items in the invoiceline table
+CREATE OR REPLACE FUNCTION get_avg_price
+RETURN NUMBER 
+as
+avg_price NUMBER ;
+BEGIN
+    SELECT AVG(unitprice)
+    INTO avg_price
+    FROM invoiceline;
+    
+    RETURN avg_price;
+END get_avg_price;
+/ 
+
+DECLARE 
+    avg_price NUMBER;
+BEGIN
+    avg_price := get_avg_price();
+    DBMS_OUTPUT.PUT_LINE('The avg price of invoiceline items: ' || avg_price);
+END;
+/
 
 --USER DEFINED TABLE VALUED FUNCTIONS
 --Task – Create a function that returns all employees who are born after 1968.
+--working with explicit cursor
+CREATE OR REPLACE FUNCTION get_born_after_1968
+    RETURN SYS_REFCURSOR -- a system reference cursor stores a results set
+IS
+    my_cursor SYS_REFCURSOR;
+BEGIN
+    OPEN my_cursor FOR
+    SELECT firstname, lastname, birthdate --stores result set in cursor
+    FROM employee
+    WHERE birthdate > DATE '1968-12-31';
+    
+    RETURN my_cursor;
+    CLOSE my_cursor;
+    
+END get_born_after_1968;
+/ 
+
+SELECT after_1968()
+FROM dual;
 
 --STORED PROCEDURES
 --In this section you will be creating and executing stored procedures. You will be creating various types of stored procedures that take input and output parameters.
 --BASIC STORED PROCEDURE
 --Task – Create a stored procedure that selects the first and last names of all the employees.
+CREATE OR REPLACE PROCEDURE get_first_and_last(
+my_cursor OUT SYS_REFCURSOR)
+IS
+BEGIN
+    OPEN my_cursor FOR 
+    SELECT firstname, lastname
+    FROM employee;
+
+    --CLOSE my_cursor;
+    END get_first_and_last;
+    / 
+
+DECLARE 
+    firstname employee.firstname%TYPE; -- ensures the artist_id var has the same datatype AS the artistid IN the artest table
+    lastname employee.lastname%TYPE;
+    my_cursor SYS_REFCURSOR;
+
+BEGIN
+    get_first_and_last(my_cursor);
+
+    LOOP 
+        FETCH my_cursor
+        INTO firstname, lastname;
+    
+        EXIT WHEN my_cursor%NOTFOUND;
+
+        DBMS_OUTPUT.PUT_LINE('firstname: ' || firstname || ', lastname: ' || lastname);
+    END LOOP ;
+    CLOSE my_cursor;
+END;
 
 --STORED PROCEDURE WITH INPUT PARAMS
 --Task – Create a stored procedure that updates the personal information of an employee.
@@ -156,7 +263,6 @@ END;
 --STORED PROCEDURE WITH OUTPUT PARAMS
 --Task – Create a stored procedure that returns the name and company of a customer.
 
---TRANSACTIONS
 --In this section you will be working with transactions. Transactions are usually nested within a stored procedure. You will also be working with handling errors in your SQL.
 --Task – Create a transaction that given a invoiceId will delete that invoice (There may be constraints that rely on this, find out how to resolve them).
 
@@ -167,17 +273,63 @@ END;
 --AFTER/FOR
 --Task - Create an after insert trigger on the employee table fired after a new record is inserted into the table.
 
+CREATE OR REPLACE TRIGGER employee_insert_trigger
+AFTER INSERT ON employee     --this logic will execute after any INSERT ON the employee table
+FOR EACH ROW                --it will do the logic FOR each row inserted
+
+BEGIN
+dbms_output.put_line( 'This triggers when an employee row is inserted into the table');
+END ;
+/
+
+
 --Task – Create an after update trigger on the album table that fires after a row is inserted in the table
+CREATE OR REPLACE TRIGGER album_update_TRIGGER
+AFTER UPDATE ON album
+FOR EACH ROW
+
+BEGIN
+dbms_output.put_line( 'This triggers after an album row is updated in the album table');
+END;
+/
+
 
 --Task – Create an after delete trigger on the customer table that fires after a row is deleted from the table.
+CREATE OR REPLACE TRIGGER customer_delete_TRIGGER
+AFTER DELETE ON customer
+FOR EACH ROW
+
+BEGIN
+dbms_output.put_line( 'This triggers after a row is deleted from the customer table');
+END ;
+/
 
 --BEFORE
 --Task – Create a before trigger that restricts the deletion of any invoice that is priced over 50 dollars.
+CREATE OR REPLACE TRIGGER conditional_invoice_delete
+BEFORE DELETE ON invoice
+FOR EACH ROW
+
+DECLARE
+invoice_price NUMBER; 
+
+BEGIN
+SELECT total 
+INTO invoice_price
+FROM invoice;
+
+IF (invoice_price > 50 )
+THEN dbms_output.put_line( 'This triggers after a row is deleted from the customer table');
+END if;
+
+END;
+/
 
 --JOINS
 --In this section you will be working with combining various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
 --INNER
 --Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+
 
 --OUTER
 --Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, lastname, invoiceId, and total.
