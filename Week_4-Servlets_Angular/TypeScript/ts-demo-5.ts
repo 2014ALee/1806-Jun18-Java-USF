1e3eeaff2dcb0f59f5bdd169f624fd08e01e4a60
wsingleton <wezley.singleton@gmail.com>
2018-07-11 20:33:21 -0400

    pushing TS notes/examples, and added more to fs-bank-demo (registration)

---
+++ Week_4-Servlets_Angular/TypeScript/ts-demo-5.ts
@@ -0,0 +1,21 @@
+/*
+    Introduction to TypeScript
+    Section 5 - Modules
+*/
+
+/*
+    Continuing on with our examples from earlier demos, we will simplify our class a bit to
+    facilitate our discussion regarding modules.
+
+    In real applications, we rarely work with a single file but instead a collection of dozens
+    or hundreds of other files. So, let's move our class declaration to another file called 
+    'Dot.ts'.
+
+    Since the class is no longer a part of this file, we need to import it in order to be 
+    able to use it. We need to specify what class(es) we are importing and the relative
+    path of the file. By including imports, TS views this file as it's own module.
+*/
+import { Dot } from './dot';
+
+let myDot = new Dot(1, 2);
+myDot.draw();
