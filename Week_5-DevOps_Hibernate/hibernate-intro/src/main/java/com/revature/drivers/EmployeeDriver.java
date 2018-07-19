9e26508f8b9c4bd4cff63e5f14ee575ae5252c75
wsingleton <wezley.singleton@gmail.com>
2018-07-19 16:03:36 -0400

    pushing hibernate intro demo from Wk5-D4

---
+++ Week_5-DevOps_Hibernate/hibernate-intro/src/main/java/com/revature/drivers/EmployeeDriver.java
@@ -0,0 +1,68 @@
+package com.revature.drivers;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import com.revature.models.Employee;
+import com.revature.util.HibernateUtil;
+
+public class EmployeeDriver {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		
+		// Create an Employee object (transient)
+		Employee emp = new Employee();
+		emp.setName("Blake");
+		emp.setRole("Developer");
+		emp.setInsertTime(new Date());
+		
+		
+		// Establish a session
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+//		// Start the transaction
+//		session.beginTransaction();
+//		
+//		// Save the Employee object to the DB
+//		session.save(emp);
+//		
+//		// Commit the transaction
+//		session.getTransaction().commit();
+//		
+//		// Since the object is saved the DB, and was in the persistent state - it has an ID now
+//		System.out.println("New employee added with id: " + emp.getId());
+//		
+//		// Terminate the SessionFactory, otherwise the application doesn't end.
+//		HibernateUtil.getSessionFactory().close();
+		
+		//--------------------------------------------------------------------------------------------
+		
+		// Start the transaction
+		session.beginTransaction();
+		
+		// Get the named query defined in the named-queries.hbm.xml
+		Query query = session.createNamedQuery("findEmployeeByName");
+		query.setParameter("name", "Wezley");
+		
+		// Run the query and save its returned value to a collection variable
+		List<Employee> employees = query.getResultList();
+		
+		// Print out employees retrieved from DB
+		for(Employee e : employees) {
+			System.out.println(e);
+		}
+		
+		// Close the transaction
+		session.getTransaction().commit();
+		
+		// Terminate the SessionFactory, otherwise the application doesn't end.
+		HibernateUtil.getSessionFactory().close();
+		
+	}
+	
+}
