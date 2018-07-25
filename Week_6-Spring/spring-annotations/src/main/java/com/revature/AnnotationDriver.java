0266ef1220dd04163e3d67c28a5d08d3b4e47d3e
wsingleton <wezley.singleton@gmail.com>
2018-07-25 15:19:36 -0400

    pushing demos from Wk6-D2 and Wk6-D3, Core Spring Framework demos

---
+++ Week_6-Spring/spring-annotations/src/main/java/com/revature/AnnotationDriver.java
@@ -0,0 +1,33 @@
+package com.revature;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.revature.beans.Coach;
+
+public class AnnotationDriver {
+	
+	public static void main(String[] args) {
+		
+		// Load the Spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		// Get beans from the container
+		Coach baseballCoach = context.getBean("myBaseballCoach", Coach.class);
+		Coach trackCoach = context.getBean("trackCoach", Coach.class);
+		Coach footballCoach = context.getBean("myFootballCoach", Coach.class);
+		
+		// Call some methods on the retrieved beans
+		System.out.println(baseballCoach.getDailyWorkout());
+		System.out.println(baseballCoach.getMotivation());
+		
+		System.out.println(trackCoach.getDailyWorkout());
+		System.out.println(trackCoach.getMotivation());
+		
+		System.out.println(footballCoach.getDailyWorkout());
+		System.out.println(footballCoach.getMotivation());
+			
+		// Close the container
+		context.close();
+	}
+
+}
