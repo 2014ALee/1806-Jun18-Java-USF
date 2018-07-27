a3c14734c49fd4cdec55378af4c242033bb00f13
Karen Matney <matneyka@gmail.com>
2018-07-27 12:27:29 -0400

    Week 5 Day 5 morning

--- Karen_Matney/Week_6/spring-rest-client/src/main/java/com/revature/ClientDriver.java
+++ Karen_Matney/Week_6/spring-rest-client/src/main/java/com/revature/ClientDriver.java
@@ -1,7 +1,13 @@
 package com.revature;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +22,18 @@ public class ClientDriver {
 
 		// Get all flash cards
 		try {
-
+			System.out.println("GET ALL FLASH CARDS");
+			ResponseEntity<FlashCard[]> response = restTemplate.getForEntity(API_URL, FlashCard[].class);
+			
+			LOG.info(response.toString());
+			System.out.println("Status Code: [" + response.getStatusCodeValue() + "] - " + response.getStatusCode());
+			System.out.println("Response Headers: " + response.getHeaders());
+			
+			System.out.println("Payload:" );
+			FlashCard[] cards = response.getBody();
+			for(FlashCard card : cards) {
+				System.out.println(card + "\n");
+			}
 		} catch(Exception e) {
 			LOG.error("Resource consumption failed");
 		}
@@ -54,6 +71,9 @@ public class ClientDriver {
 
 		// Update a flash card (Trickiest one)
 		try {
+			System.out.println("UPDATE FLASH CARD");
+			FlashCard card = new FlashCard(6,"What does AOP stand for?","Aspect-Oriented Programming");
+			// Look at HTTPHeaders
 
 		} catch(Exception e) {
 			LOG.error("Resource update failed");
@@ -61,7 +81,18 @@ public class ClientDriver {
 
 		// Delete a flash card
 		try {
+			System.out.println("DELETE FLASH CARD");
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<FlashCard> request = new HttpEntity<>(headers);
+			ResponseEntity<FlashCard> response = restTemplate.exchange(API_URL + "1", HttpMethod.DELETE, request, FlashCard.class);
+			
+			LOG.info(response.toString());
+			System.out.println("Status Code: [" + response.getStatusCodeValue() + "] - " + response.getStatusCode());
+			System.out.println("Response Headers: " + response.getHeaders());
 
+			FlashCard card = response.getBody();
+			System.out.println("Payload: " + card + "\n");
 		} catch(Exception e) {
 			LOG.error("Resource deletion failed");
 		}
